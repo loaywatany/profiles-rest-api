9eb1a86ea460d173b46da5b787bb74c66f3e648d
Hesham Watany <loay.watany@gmail.com>
2018-12-20 19:08:29 +0200

    Added permission and authentication to our UserProfileViewSet

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+
+
 from . import serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 
@@ -112,4 +117,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handels creating, creating and updating profiles."""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
     
